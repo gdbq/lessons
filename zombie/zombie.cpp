bdca96de6ea497e94db8eaf149201835b2f53a12
Parfenov <beshan@mail.ru>
2024-01-09 20:51:41 +0300

    add zombie

---
+++ zombie/zombie.cpp
@@ -0,0 +1,18 @@
+//https://github.com/gdbq/lessons/tree/main/zombie
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(){
+    pid_t child_pid;
+    child_pid = fork();
+    if(child_pid >0){
+        sleep(60);
+    }
+    else{
+        exit(0);
+    }
+    return 0;
+}
+
+
+
