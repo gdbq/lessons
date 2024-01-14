8b99566ab725c6f3c4db1f86411d603de6cea3af
Parfenov <beshan@mail.ru>
2024-01-14 17:16:12 +0300

    python 15.1 15.2

---
+++ python/15.1.py
@@ -0,0 +1,14 @@
+class Transport:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+
+class Autobus(Transport):
+    def __str__(self):
+        return f'Название автомобиля: {self.name} Скорость: {self.max_speed} Пробег: {self.mileage}'
+
+
+autobus = Autobus("Renault Logan", 180, 12)
+print(autobus)
