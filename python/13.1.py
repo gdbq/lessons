67c174911e9b300804086394569d48ba8782907c
Parfenov <beshan@mail.ru>
2024-01-23 21:37:36 +0300

    fix

--- python/13.1.py
+++ python/13.1.py
@@ -2,15 +2,19 @@ import random
 
 
 def matrix_addition(matrix_1, matrix_2):
-    return [[matrix_1[i][j] + matrix_2[i][j] for j in range(len(m1))] for i in range(len(m1[0]))]
+    return [[matrix_1[i][j] + matrix_2[i][j] for j in range(len(matrix_1[0]))] for i in range(len(matrix_1))]
 
 
 def get_random_matrix(a, b, mn, mx):
     return [[random.randint(mn, mx) for j in range(a)] for i in range(b)]
 
 
-m1 = get_random_matrix(10, 10, -5, 8)
-m2 = get_random_matrix(10, 10, -4, 8)
+a = random.randint(1, 10)
+b = random.randint(1, 10)
+print(f'{a}X{b}')
+m1 = get_random_matrix(a, b, -5, 8)
+m2 = get_random_matrix(a, b, -4, 8)
 print(m1)
 print(m2)
 print(matrix_addition(m1, m2))
+
